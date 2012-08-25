bb7cb125325bdc1aa87b60bc804fee17d3a55b56
Thomas Petrachi <guillaume.montard@vodeclic.com>
2012-08-25 02:26:49 +0200

    Space - creating project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Space::Application.config.secret_token = '47d6133ba13f4ce4b50b030f079cefb1601dc3cdc26b927c88f444c88b02df6ef2f4939662d12ca71d8c84afc3cbd88d6335c879d037ea2d9e02ac1cb9e2d9c0'
