8b60a20e9cf77fb02b0d5fa49cde9ef518504418
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2012-08-27 16:34:23 +0200

    101 - adding active_admin

---
+++ app/admin/main_users.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register MainUser do
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Space::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   root :to => "home#index"
 
   # The priority is based upon order of creation:
