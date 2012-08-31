5761d65c2726a78fc5aeac884a0dfba2e80347aa
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2012-09-01 01:40:36 +0200

    101 - remove obtrusive css & js from AA

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -126,4 +126,11 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
+  
+  config.clear_javascripts!
+  config.register_javascript 'active_admin'
+  
+  config.clear_stylesheets!
+  config.register_stylesheet 'active_admin'
+  
 end
