3c5cbd87f5279566bb5d766f0afff4104bd5248a
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2012-11-05 02:15:53 +0100

    101 - test for game (or general) layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,11 @@ class HomeController < ApplicationController
   def css_grid 
     @collection = [Enumerable, Array, String].inject([]){ |arr, cur| arr << {:name=>cur.name, :methods=>"#{ cur.methods.count } methods"} }
     @large_collection = [Hash, Set, Fixnum, Float, NilClass, TrueClass].inject([]){ |arr, cur| arr << {:name=>cur.name, :methods=>"#{ cur.methods.count } methods"} }
-    
+  end
+  
+  def game_layout_demo
+    @sections = Array.new(4,0)
+    render :layout => false
   end
   
   def preview namespace
