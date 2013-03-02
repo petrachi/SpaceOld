fb7f1a2957db231c21cf721733363138b63cff3a
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2013-03-02 07:02:38 +0100

    gems - add javascript tabs + scss variables can be overrided by app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,6 @@ module ApplicationHelper
   end
 
   def coderay options = {}, &block
-    CodeRay.scan(options.delete(:str) || capture(&block), options.delete(:lang) || :ruby).div(:css => :class).html_safe
+    CodeRay.scan(options.delete(:str) || capture(&block), options.delete(:lang) || :ruby).div(:css => :class, :tab_width => 2).html_safe
   end
 end
