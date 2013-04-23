1509a415c518c8e2c61f68ccba5ff65d58248f78
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2013-04-24 00:10:12 +0200

    blog - doing stuff

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Dir[File.join(Rails.root, "lib", "**", "*.rb")].each {|l| require l }
 
 # Initialize the rails application
 Space::Application.initialize! do |config|
-  config.autoload_paths += %W(#{ Rails.root }/app/modules)
+  config.autoload_paths += %W(#{ Rails.root }/app/concerns)
   config.autoload_paths += %W(#{ Rails.root }/app/super_user)
 end
 
