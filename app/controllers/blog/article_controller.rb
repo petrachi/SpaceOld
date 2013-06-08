203d9c5bda23f47574e95a40f49413a239dc6019
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2013-06-08 02:21:06 +0200

    blog - this will help fix

--- app/controllers/blog/article_controller.rb
+++ app/controllers/blog/article_controller.rb
@@ -33,86 +33,102 @@ class Blog::ArticleController < Blog::ApplicationController
 	      		.first
 	    
 	    scss = %q{
-	      $sc-icon-sprites: sprite-map("blog/experiment/sc-icon/*.png");
+		$sc-icon-sprites: sprite-map("blog/experiment/sc-icon/*.png");
 
-				.sc-icon-sprite{
-				    background-image: inline-sprite($sc-icon-sprites);
+		.sc-icon-sprite{
+		    background-image: inline-sprite($sc-icon-sprites);
+		}
+
+		@include sprites($sc-icon-sprites, protoss terran zerg);
+
+		@each $sprite in sprite_names($sc-icon-sprites) {
+			.sc-icon-\#{$sprite}{
+				@extend .sc-icon-sprite;
+
+				display: inline-block;
+				width: image-width(sprite_file($sc-icon-sprites, $sprite));
+				height: image-height(sprite_file($sc-icon-sprites, $sprite));
+			}
+		}
+
+		@mixin sprite-reposition-percent($map, $space-between){
+			$mapHeight: image-height(sprite-path($map)) - $space-between;
+
+			@each $sprite in sprite_names($map) {
+				$spriteHeight: nth(sprite-position($map, $sprite), 2);
+				$spriteHeight: -$spriteHeight;
+
+				.\#{sprite_map_name($map)}-\#{$sprite}{
+					background-size: 100%;
+					background-position: 50% custom_percentage($spriteHeight, $mapHeight);
+
+					display: inline-block;
 				}
+			}
+		}
 
-				@include sprites($sc-icon-sprites, protoss terran zerg);
+		@include sprite-reposition-percent($sc-icon-sprites, 264px);
 
-				@each $sprite in sprite_names($sc-icon-sprites) {
-					.sc-icon-\#{$sprite}{
-						@extend .sc-icon-sprite;
+    @mixin resize-sprite($width) {
+		  width: $width;
+			height: $width;
 
-						display: inline-block;
-						width: image-width(sprite_file($sc-icon-sprites, $sprite));
-						height: image-height(sprite_file($sc-icon-sprites, $sprite));
-					}
-				}
+			box-shadow: inset 0 0 $width/2 black;
+		}
 
-				@mixin sprite-reposition-percent($map, $space-between){
-					$mapHeight: image-height(sprite-path($map)) - $space-between;
+		@each $sprite in sprite_names($sc-icon-sprites) {
+		  .sc-icon-\#{$sprite}{
+				@include resize-sprite(66px);
+			}
 
-					@each $sprite in sprite_names($map) {
-						$spriteHeight: nth(sprite-position($map, $sprite), 2);
-						$spriteHeight: -$spriteHeight;
+			.sc-icon-\#{$sprite}.icon-x2{
+				@include resize-sprite(132px);
+			}
 
-						.\#{sprite_map_name($map)}-\#{$sprite}{
-							background-size: 100%;
-							background-position: 50% custom_percentage($spriteHeight, $mapHeight);
+    	.sc-icon-\#{$sprite}.icon-x3{
+				@include resize-sprite(198px);
+			}
 
-							display: inline-block;
-						}
-					}
-				}
-
-				@include sprite-reposition-percent($sc-icon-sprites, 264px);
-
-		    @mixin resize-sprite($width) {
-				  width: $width;
-					height: $width;
-
-					box-shadow: inset 0 0 $width/2 black;
-				}
-
-				@each $sprite in sprite_names($sc-icon-sprites) {
-				  .sc-icon-\#{$sprite}{
-						@include resize-sprite(66px);
-					}
-
-					.sc-icon-\#{$sprite}.icon-x2{
-						@include resize-sprite(132px);
-					}
-
-		    	.sc-icon-\#{$sprite}.icon-x3{
-						@include resize-sprite(198px);
-					}
-
-					.sc-icon-\#{$sprite}.icon-x4{
-						@include resize-sprite(264px);
-					}
-				}
+			.sc-icon-\#{$sprite}.icon-x4{
+				@include resize-sprite(264px);
+			}
+		}
 
 
-				#sc-demo{
-					line-height: 0;
-				  margin: 1em 0;
+		#sc-demo{
+			line-height: 0;
+		  margin: 1em 0;
 
-					.row{
-						width: 864px;
-					}
-				}
+			.row{
+				width: 864px;
+			}
+		}
 
-				#fit-article{
-				  margin: 1em 5em;
-				}
-	    }
+		#fit-article{
+		  margin: 1em 5em;
+		}
+  }
 	    
 	    @code = %Q{
 	      <%= scss %Q{#{ scss }} %>
 	    }
-    end
+=begin	    
+	    @code = %Q{
+	      <%
+          #{ experiment.version.params }
+          #{ experiment.version.ruby }
+        %>
+
+        <%= scss %Q{#{ experiment.version.scss }} %>
+
+        #{ experiment.version.erb }
+
+        <script type='text/javascript'>
+          #{ experiment.version.js }
+        </script>
+	    }
+=end	    
+	  end
   
   
   
--- app/controllers/blog/experiment_controller.rb
+++ app/controllers/blog/experiment_controller.rb
@@ -24,8 +24,106 @@ class Blog::ExperimentController < Blog::ApplicationController
   #if Rails.env == "development"
   
     def tmp
-    end
-  
+      experiment = Blog::Experiment.where(:id => 4)
+	      		.with_version(5)
+	      		.first
+	    
+	    scss = %q{
+
+		.sc-icon-sprite{
+		    background-image: inline-sprite($youpi-sprites);
+		}
+
+		@include sprites($youpi-sprites, protoss terran zerg);
+
+		@each $sprite in sprite_names($youpi-sprites) {
+			.sc-icon-\#{$sprite}{
+				@extend .sc-icon-sprite;
+
+				display: inline-block;
+				width: image-width(sprite_file($youpi-sprites, $sprite));
+				height: image-height(sprite_file($youpi-sprites, $sprite));
+			}
+		}
+
+		@mixin sprite-reposition-percent($map, $space-between){
+			$mapHeight: image-height(sprite-path($map)) - $space-between;
+
+			@each $sprite in sprite_names($map) {
+				$spriteHeight: nth(sprite-position($map, $sprite), 2);
+				$spriteHeight: -$spriteHeight;
+
+				.\#{sprite_map_name($map)}-\#{$sprite}{
+					background-size: 100%;
+					background-position: 50% custom_percentage($spriteHeight, $mapHeight);
+
+					display: inline-block;
+				}
+			}
+		}
+
+		@include sprite-reposition-percent($youpi-sprites, 264px);
+
+    @mixin resize-sprite($width) {
+		  width: $width;
+			height: $width;
+
+			box-shadow: inset 0 0 $width/2 black;
+		}
+
+		@each $sprite in sprite_names($youpi-sprites) {
+		  .sc-icon-\#{$sprite}{
+				@include resize-sprite(66px);
+			}
+
+			.sc-icon-\#{$sprite}.icon-x2{
+				@include resize-sprite(132px);
+			}
+
+    	.sc-icon-\#{$sprite}.icon-x3{
+				@include resize-sprite(198px);
+			}
+
+			.sc-icon-\#{$sprite}.icon-x4{
+				@include resize-sprite(264px);
+			}
+		}
+
+
+		#sc-demo{
+			line-height: 0;
+		  margin: 1em 0;
+
+			.row{
+				width: 864px;
+			}
+		}
+
+		#fit-article{
+		  margin: 1em 5em;
+		}
+  }
+	    
+	    @code = %Q{
+	      <%= scss %Q{#{ scss }} %>
+	    }
+=begin	    
+	    @code = %Q{
+	      <%
+          #{ experiment.version.params }
+          #{ experiment.version.ruby }
+        %>
+
+        <%= scss %Q{#{ experiment.version.scss }} %>
+
+        #{ experiment.version.erb }
+
+        <script type='text/javascript'>
+          #{ experiment.version.js }
+        </script>
+	    }
+=end	    
+	  end
   #end
   ####
 end
