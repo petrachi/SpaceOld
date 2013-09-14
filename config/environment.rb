efef01e369c2f2ee23c9c33b17a2cc66864da7ab
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2013-09-14 02:07:53 +0200

    blog - review app config

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Load .rb files in lib directory
-Dir[File.join(Rails.root, "lib", "**", "*.rb")].each {|l| require l }
-
 # Initialize the rails application
-Space::Application.initialize! do |config|
-  config.autoload_paths += %W(#{ Rails.root }/app/concerns)
-  config.autoload_paths += %W(#{ Rails.root }/app/super_user)
-end
-
-# constants
-URL = Rails.application.routes.url_helpers
+Space::Application.initialize!
---
+++ config/initializers/constants.rb
@@ -0,0 +1,1 @@
+URL = Rails.application.routes.url_helpers
