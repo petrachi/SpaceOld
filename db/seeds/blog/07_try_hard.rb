af259cd0ad5e0e43322cd565429aa548f98ce756
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2013-10-18 23:52:27 +0200

    blog - seed try hard

--- db/seeds/blog/07_try_hard.rb
+++ db/seeds/blog/07_try_hard.rb
@@ -15,3 +15,58 @@ Blog::Screencast.create :user => @primal_user.blog_user,
   :published => true,
   :tag => :try_hard_1
 
+Blog::Screencast.create :user => @primal_user.blog_user,
+  :title => "Try Hard #2",
+  :summary => "On va parler des blocks",
+  :embed => "<iframe width=\"840\" height=\"500\" src=\"//www.youtube.com/embed/8Slu1_5xUYA?rel=0\" frameborder=\"0\" allowfullscreen></iframe>",
+  :snippet => Blog::Snippet.create(:erb => %Q{
+    <p>
+      Alors voilà jean-michel, un super screencast dites donc !
+    </p>
+  },
+  :published => true),
+  :pool => :try_hard,
+  :published => true,
+  :tag => :try_hard_2
+    
+Blog::Screencast.create :user => @primal_user.blog_user,
+  :title => "Try Hard #3",
+  :summary => "On va parler de refactoring",
+  :embed => "<iframe width=\"840\" height=\"500\" src=\"//www.youtube.com/embed/s6iMBtzo3nE?rel=0\" frameborder=\"0\" allowfullscreen></iframe>",
+  :snippet => Blog::Snippet.create(:erb => %Q{
+    <p>
+      Alors voilà jean-michel, un super screencast dites donc !
+    </p>
+  },
+  :published => true),
+  :pool => :try_hard,
+  :published => true,
+  :tag => :try_hard_3
+      
+Blog::Screencast.create :user => @primal_user.blog_user,
+  :title => "Try Hard #4",
+  :summary => "On va parler des null objects",
+  :embed => "<iframe width=\"840\" height=\"500\" src=\"//www.youtube.com/embed/Z74ANuUEs7g?rel=0\" frameborder=\"0\" allowfullscreen></iframe>",
+  :snippet => Blog::Snippet.create(:erb => %Q{
+    <p>
+      Alors voilà jean-michel, un super screencast dites donc !
+    </p>
+  },
+  :published => true),
+  :pool => :try_hard,
+  :published => true,
+  :tag => :try_hard_4
+        
+Blog::Screencast.create :user => @primal_user.blog_user,
+  :title => "Try Hard #5",
+  :summary => "On va parler de method missing",
+  :embed => "<iframe width=\"840\" height=\"500\" src=\"//www.youtube.com/embed/X1bmSh72JoA?rel=0\" frameborder=\"0\" allowfullscreen></iframe>",
+  :snippet => Blog::Snippet.create(:erb => %Q{
+    <p>
+      Alors voilà jean-michel, un super screencast dites donc !
+    </p>
+  },
+  :published => true),
+  :pool => :try_hard,
+  :published => true,
+  :tag => :try_hard_5
