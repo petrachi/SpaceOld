6797ede61027cc8c2e62f241e5289cca3e77f29d
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2013-11-18 00:57:21 +0100

    blog - suite new grid

--- lib/new_grid.rb
+++ lib/new_grid.rb
@@ -1,24 +1,42 @@
 module NewGrid
   
+  def html_options options = {}, defaults = {}
+    id = options[:id]
+    classes = "#{ defaults[:class] } #{ options[:class] }"
+    classes += " off-#{ options[:offset] }" if options[:offset]
+    
+    {
+      :id => id,
+      :class => classes
+    }
+  end
+  
+  
+  # miss-list 
+  # -> proc html_options (class / id)
+  # -> row-option & col-options in row_*_tag
+  # -> container-row-col-ptions in container_*_tag
+  
   def container_tag options = {}, &block
-    content_tag :div, class: :container, &block
+    html_options = html_options(options, {class: :container})
+    content_tag options[:tag] || :div, html_options, &block
   end
   
   def row_tag options = {}, &block
-    content_tag :div, class: :row, &block
+    html_options = html_options(options, {class: :row})
+    content_tag options[:tag] || :div, html_options, &block
   end
   
   def col_tag options = {}, &block
-    content_tag :div, class: "col-#{ options[:col_size] }", &block
+    html_options = html_options(options, {class: "col-#{ options[:col_size] }"})
+    content_tag options[:tag] || :div, html_options, &block
   end
   
   1.upto 12 do |col_size|
-    define_method "col_#{ col_size }_tag" do |&block|
-      col_tag col_size: col_size, &block
+    define_method "col_#{ col_size }_tag" do |options = {}, &block|
+      col_tag options.merge(col_size: col_size), &block
     end
-  end
   
-  1.upto 6 do |col_size|
     define_method "row_#{ col_size }_tag" do |collection, &block|
       cols_buffer = collection.inject ActiveSupport::SafeBuffer.new do |safe_buffer, instance|
         safe_buffer.safe_concat send("col_#{ col_size }_tag"){ block.call(instance) }
