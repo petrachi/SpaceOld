a33c8e9f57bf1fcabe81caa6c105b66b2bb0f671
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2013-11-19 00:36:37 +0100

    blog - suite new grid - can't test with broken ruby install

--- lib/new_grid.rb
+++ lib/new_grid.rb
@@ -1,5 +1,14 @@
 module NewGrid
   
+  # i'm not happy with that method
+  # this is doing too mush for very rare use
+  # -> maybe, allow proc only for spans, and maybe put it in a "proc options" key
+  def proc_options instance, options = {}
+    options[:proc_options].each do |key, proc|
+      options[key] = value.call(instance)
+    end
+  end
+  
   def html_options options = {}, defaults = {}
     id = options[:id]
     classes = "#{ defaults[:class] } #{ options[:class] }"
@@ -27,32 +36,38 @@ module NewGrid
     content_tag options[:tag] || :div, html_options, &block
   end
   
-  def col_tag options = {}, &block
+  def col_tag instance = nil, options = {}, &block
+    proc_options instance, options
     html_options = html_options(options, {class: "col-#{ options[:col_size] }"})
-    content_tag options[:tag] || :div, html_options, &block
+    
+    content_tag(options[:tag] || :div, html_options){ block.call(instance) }
   end
   
   1.upto 12 do |col_size|
-    define_method "col_#{ col_size }_tag" do |options = {}, &block|
-      col_tag options.merge(col_size: col_size), &block
+    define_method "col_#{ col_size }_tag" do |instance = nil, options = {}, &block|
+      col_tag instance, options.merge(col_size: col_size), &block
     end
   
-    define_method "row_#{ col_size }_tag" do |collection, &block|
+    define_method "row_#{ col_size }_tag" do |collection, options = {}, &block|
+      span_options = options.delete(:spans) || {}
+      
       cols_buffer = collection.inject ActiveSupport::SafeBuffer.new do |safe_buffer, instance|
-        safe_buffer.safe_concat send("col_#{ col_size }_tag"){ block.call(instance) }
+        safe_buffer.safe_concat send("col_#{ col_size }_tag", instance, span_options, &block)
       end
       
-      row_tag{ cols_buffer }
+      row_tag(options){ cols_buffer }
     end
     
-    define_method "container_#{ col_size }_tag" do |collection, &block|
+    define_method "container_#{ col_size }_tag" do |collection, options = {}, &block|
+      row_options = options.delete(:rows) || {}
+      
       rows_buffer = collection
         .in_groups_of(12 / 1, false)
         .inject ActiveSupport::SafeBuffer.new do |safe_buffer, collection|
-          safe_buffer.safe_concat send("row_#{ col_size }_tag", collection, &block)
+          safe_buffer.safe_concat send("row_#{ col_size }_tag", collection, row_options, &block)
       end
       
-      container_tag{ rows_buffer }
+      container_tag(options){ rows_buffer }
     end
   end
 end
